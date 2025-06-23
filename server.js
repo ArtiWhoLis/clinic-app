6d1e9fa14a70abe46db9c4b03f381995cf34ddd5
ArtiWho <sukhikh_tosha@mail.ru>
2025-06-23 23:18:01 +0400

    Дата База установка

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 const app = express();
 const PORT = process.env.PORT || 3000;
 
